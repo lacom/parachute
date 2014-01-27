7074316697b8ab85d6db3c242cfa57350f94374c
Greg Buckner <gbuckner23@gmail.com>
2014-01-26 18:43:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parachute::Application.config.secret_key_base = 'f5823ae4d7ea7082e8efc8a21bd8ed99f36bce63e224427ed36fb82b3c6f9c9f2fb120057b7fd62106b528b1b8c4e95e21912160d387fa1fceb455ae64a82e81'
